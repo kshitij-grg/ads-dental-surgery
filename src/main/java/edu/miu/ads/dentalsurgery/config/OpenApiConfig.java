1b2aee325cf2413b99d3c5bcd3b9977661f3e9ba
kshitij-grg <cheetizgurung@gmail.com>
2026-04-23 16:45:14 -0500

    refactor: move swagger config to main class for better detection

--- src/main/java/edu/miu/ads/dentalsurgery/config/OpenApiConfig.java
+++ src/main/java/edu/miu/ads/dentalsurgery/config/OpenApiConfig.java
@@ -1,24 +1,8 @@
 package edu.miu.ads.dentalsurgery.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.models.OpenAPI;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(
-        info = @Info(title = "ADS Dental Surgery API", version = "1.0", description = "Professional Dental Clinic Management System API"),
-        security = @SecurityRequirement(name = "bearerAuth")
-)
-@SecurityScheme(
-        name = "bearerAuth",
-        type = SecuritySchemeType.HTTP,
-        scheme = "bearer",
-        bearerFormat = "JWT"
-)
 public class OpenApiConfig {
+    // Configuration moved to AdsDentalSurgeryApplication.java for better detection
 }
